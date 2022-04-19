88ff7f7f1e0f6f0cb865103fbb4d7a889e5febe4
jakiasultana2022 <rokeyasbd@gmail.com>
2022-04-19 12:57:56 +0600

    äbout

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -3,7 +3,13 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-           
+            <div class="card mt-5">
+  <div class="card-body">
+  <h3>Hi I'm Rokeya Sultana. Right now I'm learning web development.</h3>
+           <p>Being a web developer, i need to showcase what i can do and the best way to do that is by having my website, code with the languages or frameworks that i am  skilled at and keep doing projects. Build my reputation, it might be slow, but it will progress, there is no substitution for hard work that naturally pays off later. Be progressive with the web tools that i know, if i am very comfortable at javascript, learn a new framework related to it. Take a chance at other languages, tools, or frameworks, do my research very well. Be updated with current tech trends and most of all, keep learning and don’t let challenges discourage me.</p>
+  </div>
+</div>
+            
         </div>
     );
 };
