130671f2e6948733f9fa857ca46d060994bd3e18
jakiasultana2022 <rokeyasbd@gmail.com>
2022-04-19 12:41:03 +0600

    navbar

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Container, Nav, Navbar} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar collapseOnSelect expand="lg" sticky='top'  bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Brand as={Link} to="/">The Fixer Man</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#About">About</Nav.Link>
-      <Nav.Link href="#blogs">Blogs</Nav.Link>
+      <Nav.Link as={Link} to="/About">About</Nav.Link>
+      <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
      
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
+      {/* <Nav.Link href="#deets">More deets</Nav.Link> */}
+      <Nav.Link as={Link} to="/login">
+       Login
       </Nav.Link>
     </Nav>
   </Navbar.Collapse>
